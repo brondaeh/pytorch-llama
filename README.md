a61323da122e7b0214179822583e25d0aa33f88a
Brandon <huangbrandon2@gmail.com>
2024-02-19 16:38:28 -0800

    updated with RoPE

--- README.md
+++ README.md
@@ -3,7 +3,27 @@
 ## Introduction
 A LLaMA 2 model is implemented in the PyTorch framework from scratch.
 
+## Requirements
+1. Ensure the required libraries are installed on your system.
+```bash
+pip install -r requirements.txt
+```
+
+2. Download model weights with the official [llama](https://github.com/facebookresearch/llama.git) repository or from [LLaMA 2 7B on Hugging Face](https://huggingface.co/meta-llama/Llama-2-7b). Make sure to include the following:
+```bash
+pytorch-llama
+├── llama-2-7b
+│   ├── checklist.chk
+│   ├── consolidated.00.pth
+│   └── params.json
+├── tokenizer.model
+```
+
 ## References
-- [pytorch-llama](https://github.com/hkproj/pytorch-llama)
-- [llama](https://github.com/facebookresearch/llama.git)
-- [LLaMA 2 on Hugging Face](https://huggingface.co/meta-llama/Llama-2-7b)
+1. [pytorch-llama](https://github.com/hkproj/pytorch-llama)
+2. [llama](https://github.com/facebookresearch/llama.git)
+3. [LLaMA 2 7B on Hugging Face](https://huggingface.co/meta-llama/Llama-2-7b)
+4. [Root Mean Square Layer Normalization](https://arxiv.org/abs/1910.07467)
+5. [RoFormer: Enhanced Transformer with Rotary Position Embedding](https://arxiv.org/abs/2104.09864)
+6. [Fast Transformer Decoding: One Write-Head is All You Need](https://arxiv.org/abs/1911.02150)
+7. [GLU Variants Improve Transformer](https://arxiv.org/abs/2002.05202)
--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import math
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class ModelArgs:
     dim: int = 4096
@@ -23,6 +24,84 @@ class ModelArgs:
     max_seq_len: int = 2048
 
     device: str = None
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))     # gamma parameter
+    
+    def _norm(self, x: torch.Tensor):
+        # (batch, seq_len, dim) * (batch, seq_len, 1) = (batch, seq_len, dim)
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)      # rsqrt = 1 / sqrt(x)
+
+    def forward(self, x: torch.Tensor):
+        # (dim) * (batch, seq_len, dim) = (batch, seq_len, dim)
+        return self.weight * self._norm(x.float()).type_as(x)
+    
+
+def precompute_theta_pos_frequencies(head_dim: int, seq_len: int, device: str, theta: float = 10000.0):
+    # Check that the embedding dimension is even as specified in RoFormer paper
+    assert head_dim % 2 == 0, "Dimension must be evenly divisible by 2."
+
+    # Build theta parameters
+    # theta_i = 10000 ^ (-2(i-1)/dim) for i [1, 2, ..., dim / 2]
+    # Shape: (head_dim / 2)
+    theta_numerator = torch.arange(0, head_dim, 2).float()
+
+    # Shape: (head_dim / 2)
+    theta = 1.0 / (theta ** (theta_numerator / head_dim)).to(device)
+
+    # Construct the positions (m parameter)
+    # Shape: (seq_len)
+    m = torch.arange(seq_len, device)
+
+    # Multiply each theta by each position using outer product to get a matrix of all m_i * theta_i combinations
+    # Shape: (seq_len) outer_product (head_dim / 2) -> (seq_len, head_dim / 2)
+    freqs = torch.outer(m, theta).float()
+
+    # Compute complex polar form c = R * exp(i * m * theta), where R = 1
+    # Shape: (seq_len, head_dim / 2) -> (seq_len, head_dim / 2)
+    freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
+
+    return freqs_complex
+
+def apply_rotary_embeddings(x: torch.Tensor, freqs_complex: torch.Tensor, device: str):
+    # (batch, seq_len, h, head_dim) -> (batch, seq_len, h, head_diim / 2)
+    x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+    # (seq_len, head_dim / 2) -> (1, seq_len, 1, head_dim / 2)
+    freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2)
+    # (batch, seq_len, h, head_dim / 2) * (1, seq_len, 1, head_dim / 2) = (batch, seq_len, h, head_dim / 2)
+    x_rotated = x_complex * freqs_complex
+    # (batch, seq_len, h, head_dim / 2) -> (batch, seq_len, h, head_dim / 2, 2)
+    x_out = torch.view_as_real(x_rotated)
+    # (batch, seq_len, h, head_dim / 2, 2) -> (batch, seq_len, h, head_dim)
+    x_out = x_out.reshape(*x.shape)
+
+    return x_out.type_as(x).to(device)
+
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    pass
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+    pass
+
+
+class FeedForward(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+    pass
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+    pass
+
 
 class Transformer(nn.Module):
     def __init__(self, args: ModelArgs) -> None:
@@ -63,4 +142,8 @@ class Transformer(nn.Module):
 
         return output
     
-# To do: rotary positional embeddings
+# To do:
+    # KV Cache
+    # Self Attention
+    # Feed Forward
+    # Encoder
